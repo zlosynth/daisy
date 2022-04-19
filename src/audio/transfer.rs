c1387b1a8ddeddb6e2e2c56b404d4a78ce0a9267
Petr Horacek <hrck@protonmail.com>
2022-04-19 19:14:04 +0200

    Move SAI initialization to its own struct
    Signed-off-by: Petr Horacek <hrck@protonmail.com>

--- src/audio/transfer.rs
+++ src/audio/transfer.rs
@@ -19,7 +19,7 @@ pub type Sai1Pins = (
 pub struct Transfer {
     transmitter: Transmitter,
     receiver: Receiver,
-    sai1: hal::sai::Sai<pac::SAI1, hal::sai::I2S>,
+    audio_interface: AudioInterface,
 }
 
 impl Transfer {
@@ -33,40 +33,25 @@ impl Transfer {
     ) -> Self {
         let dma1_streams =
             dma::dma::StreamsTuple::new(unsafe { pac::Peripherals::steal().DMA1 }, dma1_rec);
-
         let transmitter = Transmitter::init_with_channel_a(dma1_streams.0, tx_buffer);
         let receiver = Receiver::init_with_channel_b(dma1_streams.1, rx_buffer);
 
-        let sai1_master_config = sai::I2SChanConfig::new(sai::I2SDir::Tx)
-            .set_frame_sync_active_high(true)
-            .set_clock_strobe(sai::I2SClockStrobe::Falling);
-        let sai1_slave_config = sai::I2SChanConfig::new(sai::I2SDir::Rx)
-            .set_sync_type(sai::I2SSync::Internal)
-            .set_frame_sync_active_high(true)
-            .set_clock_strobe(sai::I2SClockStrobe::Rising);
-        let sai1 = unsafe { pac::Peripherals::steal().SAI1 }.i2s_ch_a(
-            sai1_pins,
-            FS,
-            sai::I2SDataSize::BITS_24,
-            sai1_rec,
-            clocks,
-            I2sUsers::new(sai1_master_config).add_slave(sai1_slave_config),
-        );
+        let audio_interface = AudioInterface::init(clocks, sai1_rec, sai1_pins);
 
         Self {
             transmitter,
             receiver,
-            sai1,
+            audio_interface,
         }
     }
 
     pub fn start(&mut self) {
         let transmitter = &mut self.transmitter;
         let receiver = &mut self.receiver;
-        let sai1 = &mut self.sai1;
+        let audio_interface = &mut self.audio_interface;
 
-        receiver.start(sai1);
-        transmitter.start(sai1);
+        receiver.start(audio_interface);
+        transmitter.start(audio_interface);
     }
 
     pub fn examine_interrupt(&mut self) -> Result<State, ()> {
@@ -85,6 +70,32 @@ impl Transfer {
 pub enum State {
     HalfSent,
     FullSent,
+}
+
+struct AudioInterface(hal::sai::Sai<pac::SAI1, hal::sai::I2S>);
+
+impl AudioInterface {
+    fn init(
+        clocks: &hal::rcc::CoreClocks,
+        sai1_rec: hal::rcc::rec::Sai1,
+        sai1_pins: Sai1Pins,
+    ) -> Self {
+        let sai1_master_config = sai::I2SChanConfig::new(sai::I2SDir::Tx)
+            .set_frame_sync_active_high(true)
+            .set_clock_strobe(sai::I2SClockStrobe::Falling);
+        let sai1_slave_config = sai::I2SChanConfig::new(sai::I2SDir::Rx)
+            .set_sync_type(sai::I2SSync::Internal)
+            .set_frame_sync_active_high(true)
+            .set_clock_strobe(sai::I2SClockStrobe::Rising);
+        Self(unsafe { pac::Peripherals::steal().SAI1 }.i2s_ch_a(
+            sai1_pins,
+            FS,
+            sai::I2SDataSize::BITS_24,
+            sai1_rec,
+            clocks,
+            I2sUsers::new(sai1_master_config).add_slave(sai1_slave_config),
+        ))
+    }
 }
 
 type _Transmitter<C> = dma::Transfer<
@@ -120,7 +131,8 @@ impl Transmitter {
         ))
     }
 
-    fn start(&mut self, sai1: &mut hal::sai::Sai<pac::SAI1, hal::sai::I2S>) {
+    fn start(&mut self, audio_interface: &mut AudioInterface) {
+        let sai1 = &mut audio_interface.0;
         match self {
             Transmitter::ChannelA(dma1_str0) => {
                 dma1_str0.start(|sai1_rb| {
@@ -174,11 +186,12 @@ impl Receiver {
         ))
     }
 
-    fn start(&mut self, sai1: &mut hal::sai::Sai<pac::SAI1, hal::sai::I2S>) {
+    fn start(&mut self, audio_interface: &mut AudioInterface) {
         unsafe {
             pac::NVIC::unmask(pac::Interrupt::DMA1_STR1);
         }
 
+        let sai1 = &mut audio_interface.0;
         match self {
             Receiver::ChannelB(dma1_str1) => {
                 dma1_str1.start(|_sai1_rb| {
